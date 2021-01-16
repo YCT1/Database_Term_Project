21d085246f7d4c187490f094937fc915d3c9b79d
YCT1 <yektacan1999@gmail.com>
2021-01-16 15:55:51 +0300

    Navigation bar implemented

--- app2.py
+++ app2.py
@@ -32,16 +32,14 @@ def parties():
     persons = cur.fetchall()
     return render_template('parties.html', persons=persons)
 
-@app.route("/parties/<string:id>")
-def individual_parties(id):
-    cur = db.cursor()
-    cur.execute("SELECT * FROM parties")
-    parties = cur.fetchall()
-    for x in parties:
-        if(x[2] == id):
-            return "<h1>"+str(x[1])+ "</h1>"
-    return "Not found"
 
+@app.route("/parties/<string:id>")
+def d_individual_parties(id):
+    cur = db.cursor()
+    cur.execute("SELECT * FROM parties WHERE idParties ="+str(id))
+    party = cur.fetchone()
+
+    return render_template("ind_party.html", party = party)
 
 
 @app.route("/persons/<string:id>")
